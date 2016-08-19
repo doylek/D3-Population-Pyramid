fb69a9063bbfa9f631d34d58e3d4776b712c7fc4
Kevin Doyle <kevin@doyle.tech>
2016-08-19 12:55:33 -0400

    beautifying

--- popPyramid.js
+++ popPyramid.js
@@ -1,90 +1,94 @@
-function pyramidBuilder(data, target, height, width, style){
+function pyramidBuilder(data, target, height, width, style) {
 
-var w = width,
-    h = height;
-var margin = {
-        top: 50,
-        right: 10,
-        bottom: 20,
-        left: 10,
-        middle: 20
-    },
-    sectorWidth = w / 2 - margin.middle,
-    leftBegin = sectorWidth,
-    rightBegin = w - sectorWidth;
+    var w = width,
+        h = height;
+    var margin = {
+            top: 50,
+            right: 10,
+            bottom: 20,
+            left: 10,
+            middle: 20
+        },
+        sectorWidth = w / 2 - margin.middle,
+        leftBegin = sectorWidth,
+        rightBegin = w - sectorWidth;
 
-var style = {
-  leftBarColor: typeof leftBarColor === 'undefined' ? '6c9dc6' : leftBarColor,
-  rightBarColor: typeof rightBarColor === 'undefined' ? 'de5454' : rightBarColor,
-  toolTipBG: typeof toolTipBG === 'undefined' ? 'fefefe' : toolTipBG
-}
+    var style = {
+        leftBarColor: typeof leftBarColor === 'undefined' ? '6c9dc6' : leftBarColor,
+        rightBarColor: typeof rightBarColor === 'undefined' ? 'de5454' : rightBarColor,
+        toolTipBG: typeof toolTipBG === 'undefined' ? 'fefefe' : toolTipBG
+    }
 
-var totalPopulation = d3.sum(data, function(d) {
-        return d.male + d.female;
-    }),
-    percentage = function(d) {
-        return d / totalPopulation;
-    };
+    var totalPopulation = d3.sum(data, function(d) {
+            return d.male + d.female;
+        }),
+        percentage = function(d) {
+            return d / totalPopulation;
+        };
 
-var styleSection = d3.select(target).append('style')
-    .text('.axis line,axis path {shape-rendering: crispEdges;fill: transparent;stroke: #555;} \
+    var styleSection = d3.select(target).append('style')
+        .text('.axis line,axis path {shape-rendering: crispEdges;fill: transparent;stroke: #555;} \
     .axis text {font-size: 11px;} \
     .bar {fill-opacity: 0.5;} \
-    .bar.left {fill: #'+style.leftBarColor+';} \
+    .bar.left {fill: #' + style.leftBarColor + ';} \
     .bar.left:hover {fill: #' + colorTransform(style.leftBarColor, '333333') + ';} \
-    .bar.right {fill: #'+style.rightBarColor+';} \
+    .bar.right {fill: #' + style.rightBarColor + ';} \
     .bar.right:hover {fill: #' + colorTransform(style.rightBarColor, '333333') + ';} \
-    .tooltip {position: absolute;line-height: 1.1em;padding: 7px; margin: 3px;background: #'+style.toolTipBG+';pointer-events: none;border-radius: 6px;}')
+    .tooltip {position: absolute;line-height: 1.1em;padding: 7px; margin: 3px;background: #' + style.toolTipBG + ';pointer-events: none;border-radius: 6px;}')
 
-var region = d3.select(target).append('svg')
-    .attr('width', margin.left + w + margin.right)
-    .attr('height', margin.bottom + h + margin.top);
+    var region = d3.select(target).append('svg')
+        .attr('width', margin.left + w + margin.right)
+        .attr('height', margin.bottom + h + margin.top);
 
 
-var legend = region.append('g')
-      .attr('class','legend');
-
-    legend.append('rect')
-      .attr('class','bar left')
-      .attr('x', (w/2) - (margin.middle*3) )
-      .attr('y',12)
-      .attr('width',12)
-      .attr('height',12);
-
-    legend.append('text')
-      .attr('fill','#000')
-      .attr('x', (w/2) - (margin.middle*2))
-      .attr('y',18)
-      .attr('dy','0.32em')
-      .text('Males');
+    var legend = region.append('g')
+        .attr('class', 'legend');
 
     legend.append('rect')
-      .attr('class','bar right')
-      .attr('x', (w/2) + (margin.middle*2) )
-      .attr('y',12)
-      .attr('width',12)
-      .attr('height',12);
+        .attr('class', 'bar left')
+        .attr('x', (w / 2) - (margin.middle * 3))
+        .attr('y', 12)
+        .attr('width', 12)
+        .attr('height', 12);
 
     legend.append('text')
-      .attr('fill','#000')
-      .attr('x', (w/2) + (margin.middle*3) )
-      .attr('y',18)
-      .attr('dy','0.32em')
-      .text('Females');
+        .attr('fill', '#000')
+        .attr('x', (w / 2) - (margin.middle * 2))
+        .attr('y', 18)
+        .attr('dy', '0.32em')
+        .text('Males');
 
-var tooltipDiv = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+    legend.append('rect')
+        .attr('class', 'bar right')
+        .attr('x', (w / 2) + (margin.middle * 2))
+        .attr('y', 12)
+        .attr('width', 12)
+        .attr('height', 12);
 
-var pyramid = region.append('g')
-    .attr('class', 'inner-region')
-    .attr('transform', translation(margin.left,margin.top));
+    legend.append('text')
+        .attr('fill', '#000')
+        .attr('x', (w / 2) + (margin.middle * 3))
+        .attr('y', 18)
+        .attr('dy', '0.32em')
+        .text('Females');
+
+    var tooltipDiv = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    var pyramid = region.append('g')
+        .attr('class', 'inner-region')
+        .attr('transform', translation(margin.left, margin.top));
 
     // find the maximum data value for whole dataset
     //  since this will be shared by both of the x-axes
     var maxValue = Math.max(
-      d3.max(data, function(d) { return percentage(d.male); }),
-      d3.max(data, function(d) { return percentage(d.female); })
+        d3.max(data, function(d) {
+            return percentage(d.male);
+        }),
+        d3.max(data, function(d) {
+            return percentage(d.female);
+        })
     );
 
     // SET UP SCALES
@@ -92,92 +96,98 @@ var pyramid = region.append('g')
     // the xScale goes from 0 to the width of a region
     //  it will be reversed for the left x-axis
     var xScale = d3.scaleLinear()
-      .domain([0, maxValue])
-      .range([0, sectorWidth])
-      .nice();
+        .domain([0, maxValue])
+        .range([0, sectorWidth])
+        .nice();
 
     var xScaleLeft = d3.scaleLinear()
-      .domain([0, maxValue])
-      .range([sectorWidth, 0]);
+        .domain([0, maxValue])
+        .range([sectorWidth, 0]);
 
     var xScaleRight = d3.scaleLinear()
-      .domain([0, maxValue])
-      .range([0, sectorWidth]);
+        .domain([0, maxValue])
+        .range([0, sectorWidth]);
 
     var yScale = d3.scaleBand()
-      .domain(data.map(function(d) { return d.group; }))
-      .range([h,0], 0.1);
+        .domain(data.map(function(d) {
+            return d.group;
+        }))
+        .range([h, 0], 0.1);
 
 
     // SET UP AXES
     var yAxisLeft = d3.axisRight()
-      .scale(yScale)
-      .tickSize(4,0)
-      .tickPadding(margin.middle-4);
+        .scale(yScale)
+        .tickSize(4, 0)
+        .tickPadding(margin.middle - 4);
 
     var yAxisRight = d3.axisLeft()
-      .scale(yScale)
-      .tickSize(4,0)
-      .tickFormat('');
+        .scale(yScale)
+        .tickSize(4, 0)
+        .tickFormat('');
 
     var xAxisRight = d3.axisBottom()
-      .scale(xScale)
-      .tickFormat(d3.format('.0%'));
+        .scale(xScale)
+        .tickFormat(d3.format('.0%'));
 
-      var xAxisLeft = d3.axisBottom()
-      // REVERSE THE X-AXIS SCALE ON THE LEFT SIDE BY REVERSING THE RANGE
-      .scale(xScale.copy().range([leftBegin, 0]))
-      .tickFormat(d3.format('.0%'));
+    var xAxisLeft = d3.axisBottom()
+        // REVERSE THE X-AXIS SCALE ON THE LEFT SIDE BY REVERSING THE RANGE
+        .scale(xScale.copy().range([leftBegin, 0]))
+        .tickFormat(d3.format('.0%'));
 
     // MAKE GROUPS FOR EACH SIDE OF CHART
     // scale(-1,1) is used to reverse the left side so the bars grow left instead of right
     var leftBarGroup = pyramid.append('g')
-      .attr('transform', translation(leftBegin, 0) + 'scale(-1,1)');
+        .attr('transform', translation(leftBegin, 0) + 'scale(-1,1)');
     var rightBarGroup = pyramid.append('g')
-      .attr('transform', translation(rightBegin, 0));
+        .attr('transform', translation(rightBegin, 0));
 
     // DRAW AXES
     pyramid.append('g')
-      .attr('class', 'axis y left')
-      .attr('transform', translation(leftBegin, 0))
-      .call(yAxisLeft)
-      .selectAll('text')
-      .style('text-anchor', 'middle');
+        .attr('class', 'axis y left')
+        .attr('transform', translation(leftBegin, 0))
+        .call(yAxisLeft)
+        .selectAll('text')
+        .style('text-anchor', 'middle');
 
     pyramid.append('g')
-      .attr('class', 'axis y right')
-      .attr('transform', translation(rightBegin, 0))
-      .call(yAxisRight);
+        .attr('class', 'axis y right')
+        .attr('transform', translation(rightBegin, 0))
+        .call(yAxisRight);
 
     pyramid.append('g')
-      .attr('class', 'axis x left')
-      .attr('transform', translation(0, h))
-      .call(xAxisLeft);
+        .attr('class', 'axis x left')
+        .attr('transform', translation(0, h))
+        .call(xAxisLeft);
 
     pyramid.append('g')
-      .attr('class', 'axis x right')
-      .attr('transform', translation(rightBegin, h))
-      .call(xAxisRight);
+        .attr('class', 'axis x right')
+        .attr('transform', translation(rightBegin, h))
+        .call(xAxisRight);
 
     // DRAW BARS
     leftBarGroup.selectAll('.bar.left')
-      .data(data)
-      .enter().append('rect')
+        .data(data)
+        .enter().append('rect')
         .attr('class', 'bar left')
         .attr('x', 0)
-        .attr('y', function(d) { return yScale(d.group) + margin.middle/4; })
-        .attr('width', function(d) { return xScale(percentage(d.male)); })
-        .attr('height', (yScale.range()[0]/data.length) - margin.middle/2)
+        .attr('y', function(d) {
+            return yScale(d.group) + margin.middle / 4;
+        })
+        .attr('width', function(d) {
+            return xScale(percentage(d.male));
+        })
+        .attr('height', (yScale.range()[0] / data.length) - margin.middle / 2)
         .on("mouseover", function(d) {
             tooltipDiv.transition()
                 .duration(200)
                 .style("opacity", 0.9);
             tooltipDiv.html("<strong>Males Age " + d.group + "</strong>" +
-                            "<br />  Population: " + prettyFormat(d.male) +
-                            "<br />" + (Math.round(percentage(d.male) *1000) /10) + "% of Total")
+                    "<br />  Population: " + prettyFormat(d.male) +
+                    "<br />" + (Math.round(percentage(d.male) * 1000) / 10) + "% of Total")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
-            })
+        })
         .on("mouseout", function(d) {
             tooltipDiv.transition()
                 .duration(500)
@@ -185,57 +195,70 @@ var pyramid = region.append('g')
         });
 
     rightBarGroup.selectAll('.bar.right')
-      .data(data)
-      .enter().append('rect')
+        .data(data)
+        .enter().append('rect')
         .attr('class', 'bar right')
         .attr('x', 0)
-        .attr('y', function(d) { return yScale(d.group) + margin.middle/4; })
-        .attr('width', function(d) { return xScale(percentage(d.female)); })
-        .attr('height', (yScale.range()[0]/data.length) - margin.middle/2)
+        .attr('y', function(d) {
+            return yScale(d.group) + margin.middle / 4;
+        })
+        .attr('width', function(d) {
+            return xScale(percentage(d.female));
+        })
+        .attr('height', (yScale.range()[0] / data.length) - margin.middle / 2)
         .on("mouseover", function(d) {
             tooltipDiv.transition()
                 .duration(200)
                 .style("opacity", 0.9);
             tooltipDiv.html("<strong> Females Age " + d.group + "</strong>" +
-                            "<br />  Population: " + prettyFormat(d.male) +
-                            "<br />" + (Math.round(percentage(d.male) *1000) /10) + "% of Total")
+                    "<br />  Population: " + prettyFormat(d.male) +
+                    "<br />" + (Math.round(percentage(d.male) * 1000) / 10) + "% of Total")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
-            })
+        })
         .on("mouseout", function(d) {
             tooltipDiv.transition()
                 .duration(500)
                 .style("opacity", 0);
         });
 
-        /* HELPER FUNCTIONS */
+    /* HELPER FUNCTIONS */
 
-        // string concat for translate
-        function translation(x,y) {
-          return 'translate(' + x + ',' + y + ')';
+    // string concat for translate
+    function translation(x, y) {
+        return 'translate(' + x + ',' + y + ')';
+    }
+
+    // numbers with commas
+    function prettyFormat(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    // lighten colors
+    function colorTransform(c1, c2) {
+        var origHex = {
+                r: c1.substring(0, 2),
+                g: c1.substring(2, 4),
+                b: c1.substring(4, 6)
+            },
+            transVec = {
+                r: c2.substring(0, 2),
+                g: c2.substring(2, 4),
+                b: c2.substring(4, 6)
+            },
+            newHex = {};
+
+        function transform(d, e) {
+            var f = parseInt(d, 16) + parseInt(e, 16);
+            if (f > 255) {
+                f = 255
+            }
+            return f.toString(16)
         }
-
-        // numbers with commas
-        function prettyFormat(x) {
-          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-
-        // lighten colors
-        function colorTransform(c1, c2) {
-          var origHex = {r: c1.substring(0,2), g: c1.substring(2,4), b: c1.substring(4,6)},
-              transhex = {r: c2.substring(0,2), g: c2.substring(2,4), b: c2.substring(4,6)},
-              newHex = {r: undefined, g: undefined, b: undefined};
-          function transform(d,e) {
-            var d = parseInt(d, 16),
-                e = parseInt(e, 16),
-                f = d + e;
-                if(f > 255) {f = 255}
-                return f.toString(16)
-          }
-          newHex.r = transform(origHex.r,transhex.r)
-          newHex.g = transform(origHex.g,transhex.g)
-          newHex.b = transform(origHex.b,transhex.b)
-          return newHex.r + newHex.g + newHex.b;
-          }
+        newHex.r = transform(origHex.r, transVec.r)
+        newHex.g = transform(origHex.g, transVec.g)
+        newHex.b = transform(origHex.b, transVec.b)
+        return newHex.r + newHex.g + newHex.b;
+    }
 
 }
